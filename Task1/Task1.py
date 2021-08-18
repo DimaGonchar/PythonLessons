55e9b406ea22732b80db9f8eeb068d4b04ba0e6b
Dima <dmitriygonchar1997@gmail.com>
2021-08-18 20:58:09 +0300

    task 1

---
+++ Task1/Task1.py
@@ -0,0 +1,31 @@
+print("Enter first number:");
+firstNumber= input();
+
+print("Enter second number:");
+secondNumber= input();
+
+print ("Enter operation:");
+operation= input();
+
+if operation =="+":
+  print("Result:", firstNumber, "+", secondNumber, "=", float(firstNumber) + float(secondNumber));
+if operation =="-":
+  print("Result:", firstNumber, "-", secondNumber, "=", float(firstNumber) - float(secondNumber));
+if operation =="*":
+  print("Result:", firstNumber, "*", secondNumber, "=", float(firstNumber) * float(secondNumber));
+if operation =="/":
+  print("Result:", firstNumber, "/", secondNumber, "=", float(firstNumber) / float(secondNumber));
+
+
+  
+
+
+
+    
+
+
+
+
+
+
+
